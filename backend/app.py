6e8f3024cccc8d75a93e495459a3c2ba74000cd2
tttalexx <130794162+tttalexx@users.noreply.github.com>
2024-09-04 03:10:40 +0100

    ы

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,24 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-from flask import Flask
-from auth import auth_bp
-from views import main_bp
 
-# Указываем путь к папке с шаблонами, которая находится в frontend/templates
-app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'frontend/templates'))
+from backend.auth import auth_bp
+from backend.views import main_bp
+from backend.models import db
+
+from flask import Flask
+
+app = Flask(__name__)
 app.config.from_object('config.settings')
 
 # Подключаем маршруты из auth.py и views.py
 app.register_blueprint(auth_bp)
 app.register_blueprint(main_bp)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- config/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///fitness.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
